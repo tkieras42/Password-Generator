60430cd344feb78e3ae81fa00b8429798a0e39c6
tkieras42 <tkieras42@gmail.com>
2023-06-17 17:47:05 -0400

    fixed addselectedcharacters function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowerCase = [a, b, c, d, e, f, g, h, i, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
-var upperCase = [A, B, C, D, E, F, G, H, I, K, L, M, N, O, P, Q, R, S, T, W, X, Y, Z];
-var numericValue = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~"];
+var lowerCase = Array.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+var upperCase = Array.of("A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "W", "X", "Y", "Z");
+var numericValue = Array.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+var specialChar = Array.of("!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~");
+var selectedTypes = Array.of();
 
 // function to make the random number
 function generateNumber(max){
   return Math.floor(Math.random() * max);
 }
 
-// function to give peramiters to a random number. Pass through array or null into each
+// function to give peramiters to a random number. Pass through true or false.
 function genRndNumb(upper, lower, numeric, special){
   var rndCount = 0
-  if(upper.isarray){
+  if(upper === true){
     upperCase.length += rndCount;
   }
-  if(lower.isarray){
+  if(lower === true){
     lowerCase.length += rndCount;
   }
-  if(numeric.isarray){
+  if(numeric === true){
     numeric.length += rndCount;
   }
-  if(special.isarray){
+  if(special === true){
     special.length += rndCount;
   }
   var rndNumb = generateNumber(rndCount);
@@ -32,21 +33,27 @@ function genRndNumb(upper, lower, numeric, special){
 
 // function to create an array out of the selected character types
 function addSelectedCharacters(selectUpper, selectLower, selectNumb, selectSpecial){
-  var allTypes = [];
-  if(selectUpper.isarray){
-    allTypes.concat(ipperCase);
+  var allTypes = Array.of();
+  if(selectUpper === true){
+    allTypes.push.apply(allTypes, upperCase);
   }
-  if(selectLower.isarray){
-    allTypes.concat(lowerCase);
+  if(selectLower === true){
+    allTypes.push.apply(allTypes, lowerCase);
   }
-  if(selectNumb.isarray){
-    allTypes.concat(numericValue);
+  if(selectNumb === true){
+    allTypes.push.apply(allTypes, numericValue);
   }
-  if(selectSpecial.isarray){
-    allTypes.concat(specialChar);
+  if(selectSpecial === true){
+    allTypes.push.apply(allTypes, specialChar);
   }
-  return allTypes.length;
+  return allTypes;
 }
+
+// testing
+var num = genRndNumb(true, true, true, true);
+console.log(num)
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();

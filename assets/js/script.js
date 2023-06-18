e57440001c20c94fc6476461fea7e53f0617fcdb
tkieras42 <tkieras42@gmail.com>
2023-06-18 18:18:07 -0400

    fixed errors in sript.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -112,7 +112,7 @@ function getUserData(){
   // prompt for password length
   for(x = true; x === true; x){
     for(i = true; i === true; i){
-      var usernumberData = prompt("How long would you like your password to be? (8-128)", "Enter Number Here!");
+      var usernumberData = prompt("How long would you like your password to be? Enter a number from 8 - 128)", "Enter Number Here!");
       var userNumberConverted = parseInt(usernumberData, 10);
       if(typeof userNumberConverted === "number" && userNumberConverted >= 8 && userNumberConverted <= 128){
         passwordLength = userNumberConverted;
